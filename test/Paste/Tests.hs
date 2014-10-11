12ac832081e4159818456b3910a3173b277123fe
Julien Tanguy <julien.tanguy@jhome.fr>
2014-10-11 16:41:59 +0200

    Test createUser

--- test/Paste/Tests.hs
+++ test/Paste/Tests.hs
@@ -1,8 +1,24 @@
 module Paste.Tests where
 
-import Test.Tasty
+import           Data.Functor
+import qualified Data.Text               as T
+import qualified Test.QuickCheck.Monadic as QCM
+import           Test.Tasty
+import qualified Test.Tasty.QuickCheck   as QC
 
-import Paste
+import           Paste
 
 tests :: TestTree
-tests = undefined
+tests = testGroup "Paste.Tests" [
+        QC.testProperty "dummy" prop_dummy
+    ,   QC.testProperty "create" prop_create
+    ]
+
+prop_dummy :: String -> Bool
+prop_dummy a = reverse (reverse a) == a
+
+prop_create = QCM.monadicIO $ do
+    email <- T.pack <$> QCM.pick QC.arbitrary
+    password <- T.pack <$> QCM.pick QC.arbitrary
+    user <- QCM.run $ createUser email password
+    QCM.assert $ (checkUserPassword password user) == True
